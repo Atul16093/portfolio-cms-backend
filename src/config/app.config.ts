d4c24214a3d0349fd86394495219d1ac4a148f0d
atul16903 <atul.iwdev@gmail.com>
2026-02-12 02:09:04 +0530

    port added

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,6 @@
 import { registerAs } from '@nestjs/config';
-
+import dotenv from 'dotenv';
+dotenv.config();
 export default registerAs('app', () => ({
   port: parseInt(process.env.PORT || '3000', 10),
   nodeEnv: process.env.NODE_ENV || 'development',
